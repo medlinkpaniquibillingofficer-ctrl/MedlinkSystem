bc91c434df6045475aca65e1379aa1ee1e331624
Billing Officer <medlinkpaniqui.billingofficer@gmail.com>
2026-05-21 15:15:35 +0800

    inform user whos celebrating bday

--- MedlinkDialysisCenter/obj/Debug/net8.0/MedlinkDialysisCenter.AssemblyInfo.cs
+++ MedlinkDialysisCenter/obj/Debug/net8.0/MedlinkDialysisCenter.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedlinkDialysisCenter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9793fa14d744763432860648dbe0d8f4412b2b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43a07d0e70093b8a8eee3aab8eb08ff626929c3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedlinkDialysisCenter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedlinkDialysisCenter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
